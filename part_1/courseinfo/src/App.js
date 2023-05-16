eaac6db1f87a54215c917e0fe2bb5ee4bd7c8a8b
doteins <112283864+doteins@users.noreply.github.com>
2023-05-16 18:30:20 +0300

    Step 1.5

--- part_1/courseinfo/src/App.js
+++ part_1/courseinfo/src/App.js
@@ -38,7 +38,7 @@ const Total = (props) => {
     props.total[0].exercises +
     props.total[1].exercises +
     props.total[2].exercises;
-    
+
   return (
     <>
       <p>Number of exercises {total}</p>
@@ -48,28 +48,29 @@ const Total = (props) => {
 
 // App component
 const App = () => {
-  const course = "Half Stack application development";
-
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total total={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts} />
     </div>
   );
 };
