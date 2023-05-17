88b6fac83a497ff6002b283282d726773ad9176e
doteins <112283864+doteins@users.noreply.github.com>
2023-05-17 16:23:27 +0300

    1.8 unicafe step 3

--- part_1/unicafe/src/App.js
+++ part_1/unicafe/src/App.js
@@ -21,6 +21,32 @@ const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
+// Result line component
+// const ResultLine = (props) => {
+//   <Display text={props.text} counter={props.counter} />
+// }
+
+// Results component
+const Results = ({ good, neutral ,bad }) => {
+  // Calculate the total clicks
+  const total = good + neutral + bad;
+  // Average of good and bad votes
+  const avg = total ? (good - bad) / total : 0;
+  // Calculate the % of positive votes
+  const percentage = total ? (good / total) * 100 : 0;
+  // <ResultLine text={props.text} counter={props.counter} />
+  return (
+    <div>
+      <Display text={"Good"} counter={good} />
+      <Display text={"Neutral"} counter={neutral} />
+      <Display text={"Bad"} counter={bad} />
+      <Display text={"All"} counter={total} />
+      <Display text={"Average"} counter={avg} />
+      <Display text={"Percentage"} counter={`${percentage}%`} />
+    </div>
+  );
+};
+
 const App = () => {
   // Headers
   const firstHeader = "Give feedback below:";
@@ -50,24 +76,6 @@ const App = () => {
     setTotal(total + 1);
   };
 
-  // Average of good and bad votes
-const average = (val1, val2, val3) => {
-  if (parseInt(val3) === 0) {
-    return 0
-  }
-  const result = ((val1 - val2) / val3)
-  return result
-}
-
-// Positive votes % calculation
-const percentage = (counter, totalCounter) => {
-  if (totalCounter === 0) {
-    return 0
-  }
-  const result = (counter / totalCounter) * 100
-  return `${result} %`
-};
-
   return (
     <div>
       <Header header={firstHeader} />
@@ -76,13 +84,7 @@ const percentage = (counter, totalCounter) => {
       <Button handleClick={increaseBad} text={"Bad"} />
 
       <Header header={secondHeader} />
-      <Display text={"Good"} counter={good} />
-      <Display text={"Neutral"} counter={neutral} />
-      <Display text={"Bad"} counter={bad} />
-      <Display text={"All"} counter={total} />
-      <Display text={"Average"} counter={average(good,bad,total)} />
-      <Display text={"Percentage"} counter={percentage(good,total)} />
-      
+      <Results good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
