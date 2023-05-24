f2e288bd8b4b632e8a5de980289e5d493d17b2c9
doteins <112283864+doteins@users.noreply.github.com>
2023-05-24 21:03:52 +0300

    2.8: The Phonebook Step3

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -14,7 +14,7 @@ const Person = ({ persons }) => {
     <>
       {
         persons.map(person => {
-          return <li key={person.name}>{person.name}</li>
+          return <li key={person.name}>{person.name} {person.number}</li>
         })
       }
     </>
@@ -23,9 +23,14 @@ const Person = ({ persons }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: "040-1234567"}
+  ])
+
+  // Set new person name
   const [newName, setNewName] = useState('')
+
+  // Set new person's number
+  const [newNumber, setNewNumber] = useState('')
 
   // Handle button click event
   const addContact = (event) => {
@@ -33,33 +38,40 @@ const App = () => {
     console.log('button clicked', event.target)
 
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     // Some method checks wheter person name already exists in array or not (true || false)
-    const personInArray = persons.some(person => person.name === personObject.name)
+    const nameInArray = persons.some(person => person.name === personObject.name)
 
-    if (personInArray) {
+    if (nameInArray) {
       alert(`${newName} is already listed on your phonebook`)
     } else {
       setPersons(persons.concat(personObject))
       setNewName("")
+      setNewNumber("")
     }
   }
 
-  // Handle event handler to change update phonebook
-  const handleContactChange = (event) => {
+  // Event handler to add new person name
+  const handleNewName = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  // Event handler to add new number
+  const handleNewNumber = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <Header text={"Phonebook"}/>
       <form onSubmit={addContact}>
-        <div>
-          Name: <input value={newName} onChange={handleContactChange}/>
-        </div>
+        <div>Name: <input value={newName} onChange={handleNewName}/></div>
+        <div>Number: <input value={newNumber} onChange={handleNewNumber}/></div>
         <div>
           <button type="submit">Add</button>
         </div>
@@ -68,7 +80,6 @@ const App = () => {
       <ul>
         <Person persons={persons} />
       </ul>
-
       <div>debug: {newName}</div>
     </div>
   )
